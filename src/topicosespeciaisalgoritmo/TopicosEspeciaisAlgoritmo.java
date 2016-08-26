34f1925940860faa08db4e4bd2e384fd1dd01265
Rafa <Rafa@Rafael>
2016-08-25 22:00:56 -0300

    Alterei tudo proque estamos comecando hehehe

---
+++ src/topicosespeciaisalgoritmo/TopicosEspeciaisAlgoritmo.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package topicosespeciaisalgoritmo;
+
+/**
+ *
+ * @author Rafa
+ */
+public class TopicosEspeciaisAlgoritmo {
+    private Double s;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        
+        
+    }
+    
+   
+   
+    
+    public Double entropiaS(int pMais, int pMenos){
+        
+        
+        s = (pMais * Math.log(pMais)) - (pMenos * Math.log(pMenos));
+        
+        
+        return s;
+    
+    }
+    
+}
